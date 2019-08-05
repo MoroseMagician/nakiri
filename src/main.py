9cc8b497d0b03893208400416224ff74a9b5b0ad
magician <magician@hakke.ro>
2019-08-05 23:26:03 +0000

    SQLAlchemy and hooking into the Flask CLI

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
+import click
 from flask import Flask
+from flask.cli import AppGroup
+from flask_sqlalchemy import SQLAlchemy
 from api import auth
 from api import index
+
+db = SQLAlchemy()
 
 
 def create_app():
@@ -8,4 +13,24 @@ def create_app():
     app.register_blueprint(auth.blueprint)
     app.register_blueprint(index.blueprint)
 
+    populate_cli(app)
+
     return app
+
+
+def populate_cli(app):
+    db_cli = AppGroup('db', help='Database commands')
+
+    @db_cli.command(name='init')
+    @click.confirmation_option()
+    def db_init():
+        """ Initialize the database """
+        click.secho('Initializing database...', fg='red', color=True)
+
+    @db_cli.command(name='migrate')
+    @click.confirmation_option()
+    def db_migrate():
+        """ Run migrations """
+        click.secho('Starting migration...', fg='red', color=True)
+
+    app.cli.add_command(db_cli)
