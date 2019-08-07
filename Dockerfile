c6687154202fbeb548d4f72ae4be996c09b472c5
magician <magician@hakke.ro>
2019-08-07 21:18:23 +0000

    Docker tweaks
    Finally named the container, and changed where dependencies are
    installed

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,3 @@ COPY entrypoint.sh entrypoint.sh
 
 RUN chmod +x entrypoint.sh
 RUN pip install pipenv
-RUN pipenv install --system --deploy
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 cd /app
+pipenv install --system --deploy
 pipenv run flask db init --yes
 pipenv run flask run --host=0.0.0.0
